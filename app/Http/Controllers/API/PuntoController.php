b804ef209abef4e24584e45948da8ac913d99dfe
MammutAlex <alexk.mammut@gmail.com>
2018-07-31 16:43:21 +0300

    Create package and test width coverage

--- app/Http/Controllers/API/PuntoController.php
+++ app/Http/Controllers/API/PuntoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Package\TextCorrector;
 use App\Repositories\MessageRepo;
 use Illuminate\Routing\Controller as BaseController;
 use Telegram;
@@ -73,18 +74,7 @@ class PuntoController extends BaseController
 
     private function puntoBot($message)
     {
-        $text = $message['reply_to_message']['text'];
-        $char_array = preg_split('//u', $text, null, PREG_SPLIT_NO_EMPTY);
-        $result = '';
-        $d_fr = array('`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ',', '.', ';', "'", '[', ']', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '<', '>', ':', '"', '{', '}', '?', '/', '@', '#', '$', '^', '&');
-        $d_in = array('ё', 'ф', 'и', 'с', 'в', 'у', 'а', 'п', 'р', 'ш', 'о', 'л', 'д', 'ь', 'т', 'щ', 'з', 'й', 'к', 'ы', 'е', 'г', 'м', 'ц', 'ч', 'н', 'я', 'б', 'ю', 'ж', 'э', 'х', 'ъ', 'Ф', 'И', 'С', 'В', 'У', 'А', 'П', 'Р', 'Ш', 'О', 'Л', 'Д', 'Ь', 'Т', 'Щ', 'З', 'Й', 'К', 'Ы', 'Е', 'Г', 'М', 'Ц', 'Ч', 'Н', 'Я', 'Б', 'Ю', 'Ж', 'Э', 'Х', 'Ъ', ',', '.', '"', '№', ';', ':', '?');
-        for ($i = 0; $i < count($char_array); $i++) {
-            if (preg_match('/[А-Яа-яЁё]/u', $char_array[$i])) {
-                $result = $result . str_replace($d_in, $d_fr, $char_array[$i]);
-            } else {
-                $result = $result . str_replace($d_fr, $d_in, $char_array[$i]);
-            }
-        }
+        $result = app()->make(TextCorrector::class)->punto($message['reply_to_message']['text']);
         Telegram::sendMessage([
             'chat_id' => $message['chat']['id'],
             'text' => $result
@@ -94,18 +84,7 @@ class PuntoController extends BaseController
 
     private function capsBot($message)
     {
-        $text = $message['reply_to_message']['text'];
-        $char_array = preg_split('//u', $text, null, PREG_SPLIT_NO_EMPTY);
-        $result = '';
-        $d_fr = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ё', 'ф', 'и', 'с', 'в', 'у', 'а', 'п', 'р', 'ш', 'о', 'л', 'д', 'ь', 'т', 'щ', 'з', 'й', 'к', 'ы', 'е', 'г', 'м', 'ц', 'ч', 'н', 'я', 'б', 'ю', 'ж', 'э', 'х', 'ъ');
-        $d_in = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ё', 'Ф', 'И', 'С', 'В', 'У', 'А', 'П', 'Р', 'Ш', 'О', 'Л', 'Д', 'Ь', 'Т', 'Щ', 'З', 'Й', 'К', 'Ы', 'Е', 'Г', 'М', 'Ц', 'Ч', 'Н', 'Я', 'Б', 'Ю', 'Ж', 'Э', 'Х', 'Ъ');
-        for ($i = 0; $i < count($char_array); $i++) {
-            if (preg_match('/[А-ЯЁA-Z]/u', $char_array[$i])) {
-                $result = $result . str_replace($d_in, $d_fr, $char_array[$i]);
-            } else {
-                $result = $result . str_replace($d_fr, $d_in, $char_array[$i]);
-            }
-        }
+        $result = app()->make(TextCorrector::class)->capsLock($message['reply_to_message']['text']);
         Telegram::sendMessage([
             'chat_id' => $message['chat']['id'],
             'text' => $result
